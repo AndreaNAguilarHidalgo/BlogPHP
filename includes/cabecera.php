8ae8bb1303345b7e00d6b154006bd0676e989441
Marco Aspeitia <maspeitia.31@gmail.com>
2021-03-04 15:05:29 -0600

    meta viewprt de compatibilidad responsiva añadida a cabecera.php

--- includes/cabecera.php
+++ includes/cabecera.php
@@ -3,49 +3,52 @@
 
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <meta charset="utf-8"/>
-        <title>Blog de Videojuegos</title>
-        <link rel="stylesheet" type="text/css" href="./assets/css/style.css"/>
-    </head>
-    <body>
-        <!-- Cabecera -->
-        <header id="cabecera">
-            <!-- LOGO -->
-            <div id="logo">
-                <a href="index.php">
-                    Blog de Videojuegos
-                </a>
-            </div>
 
-            <!-- MENU -->
-            <nav id="menu">
-                <ul>
-                    <li>
-                        <a href="index.php">Inicio</a>
-                    </li>
-                    <?php 
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Blog de Videojuegos</title>
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css" />
+</head>
+
+<body>
+    <!-- Cabecera -->
+    <header id="cabecera">
+        <!-- LOGO -->
+        <div id="logo">
+            <a href="index.php">
+                Blog de Videojuegos
+            </a>
+        </div>
+
+        <!-- MENU -->
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="index.php">Inicio</a>
+                </li>
+                <?php 
                         $categorias = conseguirCategorias($db);
 
                         if(!empty($categorias)):
                             while($categoria = mysqli_fetch_assoc($categorias)): 
                     ?>
-                                <li>
-                                    <a href="categoria.php?id=<?=$categoria['id']?>"><?=$categoria['nombre']?></a>
-                                </li>
-                    <?php
+                <li>
+                    <a href="categoria.php?id=<?=$categoria['id']?>"><?=$categoria['nombre']?></a>
+                </li>
+                <?php
                             endwhile; 
                         endif; 
                     ?>
-                    <li>
-                        <a href="index.php">Sobre mí</a>
-                    </li>
-                    <li>
-                        <a href="index.php">Contacto</a>
-                    </li>
-                </ul>
-            </nav>
-            <div class="clearfix"></div>
-        </header>
+                <li>
+                    <a href="index.php">Sobre mí</a>
+                </li>
+                <li>
+                    <a href="index.php">Contacto</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="clearfix"></div>
+    </header>
 
-        <div id="contenedor">
+    <div id="contenedor">
